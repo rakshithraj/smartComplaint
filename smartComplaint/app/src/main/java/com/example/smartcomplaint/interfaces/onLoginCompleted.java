0b07010bd3d60c1aa92da29ffa2907fa9f31e8dd
rakshithraj <raj.rakshith@ymail.com>
2015-11-02 14:01:20 +0530

    duplicate
    duplicate

--- smartComplaint/app/src/main/java/com/example/smartcomplaint/interfaces/onLoginCompleted.java
+++
@@ -1,9 +0,0 @@
-package com.example.smartcomplaint.interfaces;
-
-
-/**
- * Interface to get the result in your app
- */
-public interface onLoginCompleted {
-    public void onLoginCompleted(Object result);
-}
